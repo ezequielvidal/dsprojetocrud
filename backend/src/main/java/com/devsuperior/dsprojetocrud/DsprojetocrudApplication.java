eca64568fc9b44b7bb031b13040f8e0fab6d1f48
ezequielvidal <ezequielvidal.ti@gmail.com>
2022-09-29 16:25:38 -0300

    Project created, Client entity

---
+++ backend/src/main/java/com/devsuperior/dsprojetocrud/DsprojetocrudApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsprojetocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsprojetocrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsprojetocrudApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsprojetocrud/DsprojetocrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsprojetocrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsprojetocrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
