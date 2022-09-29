1a3d1ce0337fd663e7548a7b400745daf683588c
ezequielvidal <ezequielvidal.ti@gmail.com>
2022-09-29 16:37:06 -0300

    Project created, Client entity

--- backend/src/main/java/com/devsuperior/dsprojetocrud/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/dsprojetocrud/resources/ClientResource.java
@@ -18,7 +18,7 @@ public class ClientResource {
 	@GetMapping
 	public ResponseEntity<List<Client>> findAll() {
 		List<Client> list = new ArrayList<>();
-		list.add(new Client(1L, "Ezequiel", "00000000000", 2000.00, Instant.parse("1986-08-31T23:30:00.00Z"), 0));
+		list.add(new Client(1L, "Ezequiel Vidal", "00000000000", 2000.00, Instant.parse("1986-08-31T23:30:00.00Z"), 0));
 		return ResponseEntity.ok().body(list);
 	}
 }
